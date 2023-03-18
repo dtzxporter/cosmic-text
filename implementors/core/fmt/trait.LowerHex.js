3fb3afea08be60ae750ab6199031c02ce1582f3f
jackpot51 <jackpot51@users.noreply.github.com>
2023-03-18 00:15:47 +0000

    deploy: be94a59eb8b50fca61d840871792d01b94364b8e

--- implementors/core/fmt/trait.LowerHex.js
+++ implementors/core/fmt/trait.LowerHex.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"rustybuzz":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.67.1/core/fmt/trait.LowerHex.html\" title=\"trait core::fmt::LowerHex\">LowerHex</a> for <a class=\"struct\" href=\"rustybuzz/struct.SerializeFlags.html\" title=\"struct rustybuzz::SerializeFlags\">SerializeFlags</a>"]]
+"rustybuzz":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.0/core/fmt/trait.LowerHex.html\" title=\"trait core::fmt::LowerHex\">LowerHex</a> for <a class=\"struct\" href=\"rustybuzz/struct.SerializeFlags.html\" title=\"struct rustybuzz::SerializeFlags\">SerializeFlags</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
