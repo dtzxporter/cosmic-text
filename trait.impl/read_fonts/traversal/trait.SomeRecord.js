dd3426a46640bd9c62a3b6bae37aa84ee2da9ce3
jackpot51 <jackpot51@users.noreply.github.com>
2024-01-31 13:33:38 +0000

    deploy: 0cea55630c97a47207f84e4cc3530404e17b6bfe

--- trait.impl/read_fonts/traversal/trait.SomeRecord.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"read_fonts":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
